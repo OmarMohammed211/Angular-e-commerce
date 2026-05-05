a6310c3c9af568c199e705d76e194057c4e18258
samybit <samyb.samir@gmail.com>
2026-05-05 17:42:55 +0300

    wire cart & checkout routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'cart',
+    loadComponent: () => import('./features/cart/cart').then(m => m.Cart),
+    title: 'Shopping Cart'
+  },
+  {
+    path: 'checkout',
+    loadComponent: () => import('./features/checkout/checkout').then(m => m.Checkout),
+    title: 'Checkout'
+  },
+];
